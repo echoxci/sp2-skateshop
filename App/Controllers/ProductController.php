c66a1f483fe953feb39bd709f0de09d89a84ce93
echoxci <frederik.veres.tr@gmail.com>
2021-12-01 20:10:29 +0100

    Removed unused code

--- App/Controllers/ProductController.php
+++ App/Controllers/ProductController.php
@@ -49,101 +49,16 @@ class ProductController extends AControllerBase
         ));
     }
 
-//    public function add()
-//    {
-//
-//        $postData = $this->request()->getPost();
-//
-//        $name = $postData['name'];
-//        $description = $postData['description'];
-//        $price = $postData['price'];
-//        $imgPath = $postData['img_path'];
-//
-//        $validData = $name && $description && $price && $imgPath;
-//
-//        if (!empty($postData) && $validData) {
-//
-//            $productModel = new ProductModel();
-//            $productModel
-//                ->setName($name)
-//                ->setDescription($description)
-//                ->setPrice($price)
-//                ->setImgPath($imgPath)
-//                ->save();
-//        }
-//
-//        header('location:/?c=home&a=skateboards');
-//    }
-//
-//    public function index()
-//    {
-//        return $this->html(($this->)
-//
-//        );
-//    }
+    public function delete()
+    {
+        $id = $this->request()->getGet()['id'] ?? null;
 
+        if ($id) {
+            try {
+                SkateboardModel::getOne($id)->delete();
+            } catch (\Exception $e) {}
+        }
 
-
-//    public function skateboards()
-//    {
-//        return $this->html(CategoriesHelper::mergeCategories(SkateboardModel::getAll()));
-//    }
-//
-//    public function boards()
-//    {
-//        return $this->html(BoardModel::getAll());
-//    }
-//
-//    public function trucks()
-//    {
-//        return $this->html(TrucksModel::getAll());
-//    }
-//
-//    public function wheels()
-//    {
-//        return $this->html(WheelsModel::getAll());
-//    }
-//
-//    public function bearings()
-//    {
-//        return $this->html(BearingsModel::getAll());
-//    }
-//
-//    public function addProduct()
-//    {
-//        $postData = $this->request()->getPost();
-//
-//        $name = $postData['name'];
-//        $description = $postData['description'];
-//        $price = $postData['price'];
-//        $imgPath = $postData['img_path'];
-//
-//        $validData = $name && $description && $price && $imgPath;
-//
-//        if (!empty($postData) && $validData) {
-//
-//            $skateboardModel = new SkateboardModel();
-//            $skateboardModel
-//                ->setName($name)
-//                ->setDescription($description)
-//                ->setPrice($price)
-//                ->setImgPath($imgPath)
-//                ->save();
-//        }
-//
-//        header('location:/?c=home&a=skateboards');
-//    }
-//
-//    public function deleteProduct()
-//    {
-//        $id = $this->request()->getGet()['id'] ?? null;
-//
-//        if ($id) {
-//            try {
-//                SkateboardModel::getOne($id)->delete();
-//            } catch (\Exception $e) {}
-//        }
-//
-//        header('location:/?c=home&a=skateboards');
-//    }
+        header('location:/?c=home&a=skateboards');
+    }
 }
