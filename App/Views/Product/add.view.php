d226f17e9b50bcbed99b7adff68aeb5d0d9b343f
echoxci <frederik.veres.tr@gmail.com>
2021-12-01 22:16:46 +0100

    Front-end stuff

--- App/Views/Product/add.view.php
+++ App/Views/Product/add.view.php
@@ -1,7 +1,7 @@
 <header class="masthead py-5">
     <div class="container px-4 px-lg-5 my-5">
         <div class="text-center text-white">
-            <h1 class="display-4 fw-bolder text-white">Extreme Skateshop</h1>
+            <h1 class="display-4 fw-bolder text-white">Pridaj produkt</h1>
             <hr class="divider">
         </div>
     </div>
